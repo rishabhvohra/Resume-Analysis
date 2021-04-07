b4d9c88e7cc92bc88955d264711bda79b942d0f7
rishabhvohra <78970440+rishabhvohra@users.noreply.github.com>
2021-04-07 11:49:15 +0530

    Update Jobseekerlogin.java

--- src/java/Jobseekerlogin.java
+++ src/java/Jobseekerlogin.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -16,22 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author jsjee
- */
 public class Jobseekerlogin extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
